0db729907f3cedfd8bc7ffcf6809c461595ce47b
Vladyslav Kalynchenko <vladyslav.kalynchenko@gmail.com>
2024-03-08 14:59:23 +0100

    Added avg function

--- FisrtSolution/Program.cs
+++ FisrtSolution/Program.cs
@@ -1,6 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine(GetAvg([1,2,3,4,5,6,7,8,9,10]));
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+static double GetAvg(int[] arr)
+{
+    int sum = 0;
+    foreach (var num in arr)
+    {
+        sum += num;
+    }
+
+    return (double)sum / arr.Length;
+}
