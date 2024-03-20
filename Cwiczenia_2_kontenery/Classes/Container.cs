f492907ea18bb12d1514c570937518c60d8e930f
Vladyslav Kalynchenko <vladyslav.kalynchenko@gmail.com>
2024-03-20 18:34:52 +0100

    Remade Load function, changed fields types from into into double

--- Cwiczenia_2_kontenery/Classes/Container.cs
+++ Cwiczenia_2_kontenery/Classes/Container.cs
@@ -6,10 +6,11 @@ public abstract class Container
 {
     private static int _numberOfContainers = 0;
     public int Id { get; set; }
-    public int MaxMass { get; set; }
-    public int Height { get; set; }
-    public int Wight { get; set; }
-    public int Weight { get; set; }
+    public double CargoWeight { get; set; }
+    public double Height { get; set; }
+    public double Wight { get; set; }
+    public double ContainerWeight { get; set; }
+    public double MaxWeight { get; set; }
     protected char Type{get; set;}
 
     protected Container()
@@ -18,27 +19,43 @@ public abstract class Container
         Id = _numberOfContainers;
     }
     
-    public void Load(int mass)
-    {
-        if (Weight + mass > MaxMass)
-        {
-            throw new OverfillException("To much load");
-        }
-        Weight += mass;
-    }
-    
-    protected Container(int maxMass, int height, int wight, int weight)
+    protected Container(double maxWeight, double height, double wight, double containerWeight)
     {
         _numberOfContainers++;
         Id = _numberOfContainers;
-        MaxMass = maxMass;
+        MaxWeight = maxWeight;
+        CargoWeight = 0;
         Height = height;
         Wight = wight;
-        Weight = weight;
+        ContainerWeight = containerWeight;
     }
+    
+    public void Load(double mass)
+    {
+        if (Weight + mass > MaxWeight)
+        {
+            throw new OverfillException("To much load");
+        }
+        CargoWeight += mass;
+    }
+    
+    public void Unload(double mass)
+    {
+        if (CargoWeight - mass < 0)
+        {
+            throw new OverfillException("To much unload");
+        }
+        CargoWeight -= mass;
+    }
+    
     public string Number
     {
         get => $"KON-{Type}-{Id}";
+    }
+    
+    public double Weight
+    {
+        get => ContainerWeight + CargoWeight;
     }
 
 }
