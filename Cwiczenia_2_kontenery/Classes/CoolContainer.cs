00ddac1a3c235f474d8123262b73aa612ade5bfe
Vladyslav Kalynchenko <vladyslav.kalynchenko@gmail.com>
2024-03-20 18:35:58 +0100

    Updated in order ro match Container.cs, added ToString and Cargo field

--- Cwiczenia_2_kontenery/Classes/CoolContainer.cs
+++ Cwiczenia_2_kontenery/Classes/CoolContainer.cs
@@ -3,9 +3,27 @@ namespace Cwiczenia_2_kontenery.Classes;
 public class CoolContainer : Container
 {
     private const char ContainerType = 'C';
+    
+    public Cargo Cargo { get;}
 
-    public CoolContainer(int maxMass, int height, int wight, int weight) : base(maxMass, height, wight, weight)
+    public CoolContainer(
+        double maxWeight, double height, double wight, double containerWeight, Cargo cargo
+        ) : base(maxWeight, height, wight, containerWeight)
     {
         Type = ContainerType;
+        Cargo = cargo;
+    }
+
+    public string CargoName {
+        get=> Cargo.Name;
+    }
+    
+    public double CargoTemperature {
+        get=> Cargo.Temperature;
+    }
+    
+    public override string ToString()
+    {
+        return $"Container ID: {Id}, Type: {Type}, Weight: {Weight}, Number: {Number}, Cargo: {CargoName}, Temperature: {CargoTemperature}";
     }
 }
