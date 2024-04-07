390a2432b08513b5e36fe4177f4695681e44081a
IImures <vlad.kalinchenko215@gmail.com>
2024-04-07 13:39:50 +0200

    Added RecordEntity and rest for it

---
+++ REST API CWICZNIA 4/REST API CWICZNIA 4/Record/Controller/RecordController.cs
@@ -0,0 +1,50 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using REST_API_CWICZNIA_4.Record.Controller.Request;
+using REST_API_CWICZNIA_4.Record.Service;
+
+namespace REST_API_CWICZNIA_4.Record.Controller;
+
+[ApiController]
+[Route("api/records")]
+public class RecordController : ControllerBase
+{
+    private RecordService _recordService;
+    
+    public RecordController(RecordService recordService)
+    {
+        _recordService = recordService;
+    }
+    
+    [HttpGet]
+    public IActionResult GetRecords()
+    {
+        return Ok(_recordService.GetAll());
+    }
+    
+    [HttpGet("{id}")]
+    public IActionResult GetRecord(int id)
+    {
+        return Ok(_recordService.GetById(id));
+    }
+    
+    [HttpPut]
+    public IActionResult CreateRecord(RecordRequest recordRequest)
+    {
+        _recordService.CreateRecord(recordRequest);
+        return Ok();
+    }
+    
+    [HttpPatch("{id}")]
+    public IActionResult UpdateRecord(int id, RecordRequest recordRequest)
+    {
+        return Ok(_recordService.UpdateRecord(id, recordRequest));
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteRecord(int id)
+    {
+        _recordService.DeleteRecord(id);
+        return NoContent();
+    }
+}
---
+++ REST API CWICZNIA 4/REST API CWICZNIA 4/Record/Repository/RecordRepository.cs
@@ -0,0 +1,14 @@
+﻿using REST_API_CWICZNIA_4.DB;
+using REST_API_CWICZNIA_4.Record.Entity;
+using REST_API_CWICZNIA_4.Repository;
+
+namespace REST_API_CWICZNIA_4.Record.Repository;
+
+public class RecordRepository : SimpleRepository<RecordEntity, ICollection<RecordEntity>>
+{
+    private IMocDb _db;
+    public RecordRepository(IMocDb db) : base(db.Records)
+    {
+        _db = db;
+    }
+}
